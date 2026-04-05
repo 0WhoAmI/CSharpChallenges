6e97a8b599a21bb8b44774c088f00f3ca8f3dd86
0WhoAmI <bartlomiej.krzysztof.studia@gmail.com>
2026-04-06 00:46:38 +0200

    feat(leetcode): solve Robot Return to Origin problem

---
+++ src/CSharpChallenges/LeetCode/Easy/RobotReturnToOriginSolution.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpChallenges.LeetCode.Easy
+{
+    /// <summary>
+    /// Task: Robot Return to Origin (LeetCode #657)
+    /// URL: https://leetcode.com/problems/robot-return-to-origin/description/?envType=daily-question&envId=2026-04-05
+    /// Complexity: Time O(n), Space O(1)
+    /// </summary>
+    public class RobotReturnToOriginSolution
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int x = 0;
+            int y = 0;
+            foreach (char move in moves)
+            {
+                switch (move)
+                {
+                    case 'U': x++; break;
+                    case 'D': x--; break;
+                    case 'R': y++; break;
+                    case 'L': y--; break;
+                }
+            }
+            return x == 0 && y == 0;
+        }
+    }
+}
---
+++ tests/CSharpChallenges.Tests/LeetCode/Easy/RobotReturnToOriginSolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using CSharpChallenges.LeetCode.Easy;
+
+namespace CSharpChallenges.Tests.LeetCode.Easy
+{
+    public class RobotReturnToOriginSolutionTests
+    {
+        [Theory]
+        [InlineData("UD", true)]
+        [InlineData("LL", false)]
+        public void JudgeCircle_ShouldReturnStartingPosition(string moves, bool expected)
+        {
+            // Arrange
+            var solution = new RobotReturnToOriginSolution();
+
+            // Act
+            var result = solution.JudgeCircle(moves);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
