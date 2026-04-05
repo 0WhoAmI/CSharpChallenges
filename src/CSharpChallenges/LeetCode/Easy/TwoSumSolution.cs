f832c1fb88a741de0fb496db0f688aebef300492
0WhoAmI <bartlomiej.krzysztof.studia@gmail.com>
2026-04-05 23:26:42 +0200

    feat(leetcode): solve Two Sum problem

---
+++ src/CSharpChallenges/LeetCode/Easy/TwoSumSolution.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpChallenges.LeetCode.Easy
+{
+    /// <summary>
+    /// Task: Two Sum (LeetCode #1)
+    /// URL: https://leetcode.com/problems/two-sum/
+    /// Complexity: Time O(n), Space O(n)
+    /// </summary>
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            // Słownik przechowujący wartość liczby i jej indeks
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                // Jeśli znaleźliśmy brakującą liczbę w słowniku, zwracamy oba indeksy
+                if (dict.ContainsKey(complement))
+                {
+                    return new int[] { dict[complement], i };
+                }
+
+                // W przeciwnym razie dodajemy obecną liczbę do słownika
+                dict[nums[i]] = i;
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
---
+++ tests/CSharpChallenges.Tests/LeetCode/Easy/TwoSumSolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using CSharpChallenges.LeetCode.Easy;
+
+namespace CSharpChallenges.Tests.LeetCode.Easy
+{
+    public class TwoSumSolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        public void TwoSum_ShouldReturnCorrectIndices(int[] nums, int target, int[] expected)
+        {
+            // Arrange
+            var solution = new TwoSumSolution();
+
+            // Act
+            var result = solution.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
