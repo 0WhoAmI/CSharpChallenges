3aea87b6516d2f9dfe80d4b286d8ec15f66aa062
0WhoAmI <bartlomiej.krzysztof.studia@gmail.com>
2026-04-06 22:11:51 +0200

    feat(leetcode): solve Walking Robot Simulation problem

---
+++ src/CSharpChallenges/LeetCode/Medium/WalkingRobotSimulationSolution.cs
@@ -0,0 +1,66 @@
+﻿namespace CSharpChallenges.LeetCode.Medium
+{
+    /// <summary>
+    /// Task: Walking Robot Simulation (LeetCode #874)
+    /// URL: https://leetcode.com/problems/walking-robot-simulation/description/?envType=daily-question&envId=2026-04-06
+    /// Complexity: Time O(N + K), Space O(K) (where N = commands, K = obstacles)
+    /// </summary>
+    public class WalkingRobotSimulationSolution
+    {
+        public int RobotSim(int[] commands, int[][] obstacles)
+        {
+            HashSet<(int, int)> obstacleSet = new HashSet<(int, int)>();
+            foreach (var obs in obstacles)
+            {
+                obstacleSet.Add((obs[0], obs[1]));
+            }
+
+            // Tablice kierunków
+            // [0] Północ: Y + 1 (Idziemy w górę)
+            // [1] Wschód: X + 1 (Idziemy w prawo)
+            // [2] Południe: Y - 1 (Idziemy w dół)
+            // [3] Zachód: X - 1 (Idziemy w lewo)
+            int[] dirX = [0, 1, 0, -1];
+            int[] dirY = [1, 0, -1, 0];
+
+            int x = 0;
+            int y = 0;
+            int direction = 0;
+            int maxDistanceSquared = 0;
+
+            foreach (int command in commands)
+            {
+                if (command == -2) // Skręt w lewo
+                {
+                    direction = (direction + 3) % 4;
+                }
+                else if (command == -1) // Skręt w prawo
+                {
+                    direction = (direction + 1) % 4;
+                }
+                else
+                {
+                    for (int step = 0; step < command; step++)
+                    {
+                        int nextX = x + dirX[direction];
+                        int nextY = y + dirY[direction];
+
+                        // Sprawdzamy czy na nowej pozycji jest przeszkoda
+                        if (obstacleSet.Contains((nextX, nextY)))
+                        {
+                            break;
+                        }
+
+                        x = nextX;
+                        y = nextY;
+
+                        // x * x jest szybsze procesorowo niż Math.Pow(x, 2)
+                        maxDistanceSquared = Math.Max(maxDistanceSquared, x * x + y * y);
+                    }
+                }
+            }
+
+            return maxDistanceSquared;
+        }
+    }
+}
---
+++ tests/CSharpChallenges.Tests/LeetCode/Medium/WalkingRobotSimulationSolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using CSharpChallenges.LeetCode.Medium;
+
+namespace CSharpChallenges.Tests.LeetCode.Medium
+{
+    public class WalkingRobotSimulationSolutionTests
+    {
+        public static IEnumerable<object[]> GetRobotSimData()
+        {
+            // Używam Array.Empty<int[]>(), co jest bardziej optymalne niż new int[][] { }
+            yield return new object[] { new int[] { 4, -1, 3 }, Array.Empty<int[]>(), 25 };
+            yield return new object[] { new int[] { 4, -1, 4, -2, 4 }, new int[][] { new int[] { 2, 4 } }, 65 };
+            yield return new object[] { new int[] { 6, -1, -1, 6 }, new int[][] { new int[] { 0, 0 } }, 36 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetRobotSimData))]
+        public void RobotSim_ShouldReturnMaximumSquaredEuclideanDistance(int[] commands, int[][] obstacles, int maxDistance)
+        {
+            // Arrange
+            var solution = new WalkingRobotSimulationSolution();
+
+            // Act
+            var result = solution.RobotSim(commands, obstacles);
+
+            // Assert
+            Assert.Equal(maxDistance, result);
+        }
+    }
+}
